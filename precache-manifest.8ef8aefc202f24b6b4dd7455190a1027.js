a37d9fe4292d4d8a3069cfb8b9d56b29d404b552
turtelian <dsadwe@vp.pl>
2020-07-10 22:57:25 +0200

    Updates

---
+++ precache-manifest.8ef8aefc202f24b6b4dd7455190a1027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3513e2547c2557b80d2",
+    "url": "/decoder/static/css/main.1304d0e2.chunk.css"
+  },
+  {
+    "revision": "c3513e2547c2557b80d2",
+    "url": "/decoder/static/js/main.07b4ed51.chunk.js"
+  },
+  {
+    "revision": "66a79ca6c59b08951081",
+    "url": "/decoder/static/js/runtime~main.b728153c.js"
+  },
+  {
+    "revision": "b0f38434e41848966f2f",
+    "url": "/decoder/static/js/2.6bf77090.chunk.js"
+  },
+  {
+    "revision": "27d95a16e52dc36bcd8bacf15504b164",
+    "url": "/decoder/index.html"
+  }
+];
